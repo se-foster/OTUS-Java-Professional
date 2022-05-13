924706db031fb7f1622dad49cd6f320a0f38ac3c
Sergei Foster <dm@sefoster.ru>
2022-05-13 12:31:14 +0300

    refactor after review

--- hw03-test-framework/src/main/java/ru/otus/TestFramework.java
+++ hw03-test-framework/src/main/java/ru/otus/TestFramework.java
@@ -1,40 +1,50 @@
 package ru.otus;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestFramework {
-    public static void runTests(String className) throws ClassNotFoundException, NoSuchMethodException,
-            InvocationTargetException, InstantiationException, IllegalAccessException {
-        List<Method> beforeMethods = new ArrayList<>();
-        List<Method> testMethods = new ArrayList<>();
-        List<Method> afterMethods = new ArrayList<>();
-        Class<?> clazz = Class.forName(className);
 
-        collectAnnotations(clazz, beforeMethods, afterMethods, testMethods);
-        checkAnnotationsAmount(beforeMethods, afterMethods);
-        executeTests(clazz, beforeMethods, afterMethods, testMethods);
+    private final Class<?> clazz;
+    private final List<Method> beforeMethods;
+    private final List<Method> testMethods;
+    private final List<Method> afterMethods;
+
+    public TestFramework(Class<?> clazz) {
+        this.clazz = clazz;
+        this.beforeMethods = new ArrayList<>();
+        this.testMethods = new ArrayList<>();
+        this.afterMethods = new ArrayList<>();
     }
 
-    private static void collectAnnotations(Class<?> clazz, List<Method> beforeMethods, List<Method> afterMethods,
-                                           List<Method> testMethods) {
+    public static void runTests(String className) throws ClassNotFoundException, NoSuchMethodException,
+            InvocationTargetException, InstantiationException, IllegalAccessException {
+        TestFramework testFramework = new TestFramework(Class.forName(className));
+        testFramework.run();
+    }
+
+    private void run() throws InvocationTargetException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException {
+        collectAnnotations();
+        checkAnnotationsAmount();
+        executeTests();
+    }
+
+    private void collectAnnotations() {
         for (Method method : clazz.getMethods()) {
-            Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
-            for (Annotation annotation : declaredAnnotations) {
-                if (annotation.annotationType().equals(Before.class)) {
-                    beforeMethods.add(method);
-                } else if (annotation.annotationType().equals(After.class)) {
-                    afterMethods.add(method);
-                } else if (annotation.annotationType().equals(Test.class))
-                    testMethods.add(method);
+            if (method.isAnnotationPresent(Before.class)) {
+                beforeMethods.add(method);
+            } else if (method.isAnnotationPresent(After.class)) {
+                afterMethods.add(method);
+            } else if (method.isAnnotationPresent(Test.class)) {
+                testMethods.add(method);
             }
         }
     }
 
-    private static void checkAnnotationsAmount(List<Method> beforeMethods, List<Method> afterMethods) {
+    private void checkAnnotationsAmount() {
         if (beforeMethods.size() > 1) {
             throw new IllegalArgumentException("В тесте может быть не более одного метода @Before");
         }
@@ -43,8 +53,7 @@ public class TestFramework {
         }
     }
 
-    private static void executeTests(Class<?> clazz, List<Method> beforeMethods, List<Method> afterMethods,
-                                     List<Method> testMethods)
+    private void executeTests()
             throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         int succeed = 0;
         int failed = 0;
@@ -66,7 +75,7 @@ public class TestFramework {
         printStatistic(succeed, failed);
     }
 
-    private static void printStatistic(int succeed, int failed) {
+    private void printStatistic(int succeed, int failed) {
         int total = succeed + failed;
         System.out.println("Выполнено тестов: " + total + " успешно: " + succeed + ", провалено: " + failed);
     }
