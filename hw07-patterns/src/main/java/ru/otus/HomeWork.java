b2bc21e7e1b5518b2e1613f37319d91fe640c04a
Sergei Foster <dm@sefoster.ru>
2022-06-09 00:01:15 +0300

    initial commit

--- hw07-patterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-patterns/src/main/java/ru/otus/HomeWork.java
@@ -1,5 +1,14 @@
 package ru.otus;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.processor.homework.ProcessorChangeFields;
+import ru.otus.processor.homework.ProcessorSecond;
+
 public class HomeWork {
 
     /*
@@ -7,7 +16,7 @@ public class HomeWork {
        1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
        2. Сделать процессор, который поменяет местами значения field11 и field12
        3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
-             Секунда должна определяьться во время выполнения.
+             Секунда должна определяться во время выполнения.
              Тест - важная часть задания
              Обязательно посмотрите пример к паттерну Мементо!
        4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
@@ -18,7 +27,27 @@ public class HomeWork {
     public static void main(String[] args) {
         /*
            по аналогии с Demo.class
-           из элеменов "to do" создать new ComplexProcessor и обработать сообщение
+           из элементов "to do" создать new ComplexProcessor и обработать сообщение
          */
+
+        var processors = List.of(new ProcessorChangeFields(),
+                new ProcessorSecond(() -> LocalDateTime.now().getSecond()));
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {});
+        var historyListener = new HistoryListener();
+        complexProcessor.addListener(historyListener);
+
+        var message = new Message.Builder(1L)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field11("field11")
+                .field12("field12")
+                .build();
+
+        var result = complexProcessor.handle(message);
+        System.out.println("result:" + result);
+
+        complexProcessor.removeListener(historyListener);
     }
 }
--- hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,17 +2,22 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private final Map<Long, Message> messages = new HashMap<>();
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        messages.put(msg.getId(), msg);
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(messages.get(id));
     }
 }
