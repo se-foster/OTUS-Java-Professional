fc068b6b5b0cb1a722c0fcc0489742475058d343
Sergei Foster <dm@sefoster.ru>
2022-06-16 10:38:10 +0300

    initial commit

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
 import ru.otus.model.Measurement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
+
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
+    /**
+     * читает файл, парсит и возвращает результат
+     */
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        List<Measurement> result;
+        Gson gson = new Gson();
+        try (JsonReader reader = Json.createReader(ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName))) {
+            String jsonString = reader.read().toString();
+            result = Arrays.asList(gson.fromJson(jsonString, Measurement[].class));
+        }
+        return result;
     }
 }
