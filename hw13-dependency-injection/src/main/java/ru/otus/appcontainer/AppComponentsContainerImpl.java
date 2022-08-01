561678c52305300089186e65f9444e72bb260a1a
Sergei Foster <dm@sefoster.ru>
2022-08-01 17:10:00 +0300

    hw13 - di

--- hw13-dependency-injection/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-dependency-injection/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,11 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -10,13 +13,37 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException {
         checkConfigClass(configClass);
-        // You code here...
+        Object obj = configClass.getDeclaredConstructor().newInstance();
+        List<Method> methods = Arrays.stream(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order()))
+                .toList();
+        for (Method method : methods) {
+            String name = method.getAnnotation(AppComponent.class).name();
+            var component = createComponent(obj, method);
+            appComponentsByName.put(name, component);
+            appComponents.add(component);
+        }
+    }
+
+    private Object createComponent(Object obj, Method method) {
+        try {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            Object[] args = Arrays.stream(parameterTypes)
+                    .map(this::getAppComponent)
+                    .toArray();
+            return method.invoke(obj, args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +54,15 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream()
+                .filter(component -> componentClass.isAssignableFrom(component.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(String.format("Component with type %s not found!", componentClass)));
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) Optional.ofNullable(appComponentsByName.get(componentName))
+                .orElseThrow(() -> new NoSuchElementException(String.format("Component with name %s not found!", componentName)));
     }
 }
--- hw13-dependency-injection/src/test/java/ru/otus/AppTest.java
+++ hw13-dependency-injection/src/test/java/ru/otus/AppTest.java
@@ -1,25 +1,23 @@
 package ru.otus;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.otus.appcontainer.AppComponentsContainerImpl;
-import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.config.AppConfig;
-import ru.otus.services.*;
+import ru.otus.services.EquationPreparer;
+import ru.otus.services.IOService;
+import ru.otus.services.PlayerService;
 
 import java.io.PrintStream;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
 
-    @Disabled //надо удалить
     @DisplayName("Из контекста тремя способами должен корректно доставаться компонент с проставленными полями")
     @ParameterizedTest(name = "Достаем по: {0}")
     @CsvSource(value = {"GameProcessor, ru.otus.services.GameProcessor",
@@ -38,7 +36,8 @@ class AppTest {
             "EquationPreparerImpl, ru.otus.services.EquationPreparer",
             "equationPreparer, ru.otus.services.EquationPreparer"
     })
-    public void shouldExtractFromContextCorrectComponentWithNotNullFields(String classNameOrBeanId, Class<?> rootClass) throws Exception {
+    public void shouldExtractFromContextCorrectComponentWithNotNullFields(String classNameOrBeanId,
+                                                                          Class<?> rootClass) throws Exception {
         var ctx = new AppComponentsContainerImpl(AppConfig.class);
 
         assertThat(classNameOrBeanId).isNotEmpty();
@@ -57,13 +56,12 @@ class AppTest {
         var fields = Arrays.stream(component.getClass().getDeclaredFields())
                 .filter(f -> !Modifier.isStatic(f.getModifiers()))
                 .peek(f -> f.setAccessible(true))
-                .collect(Collectors.toList());
+                .toList();
 
-        for (var field: fields){
+        for (var field : fields) {
             var fieldValue = field.get(component);
             assertThat(fieldValue).isNotNull().isInstanceOfAny(IOService.class, PlayerService.class,
                     EquationPreparer.class, PrintStream.class, Scanner.class);
         }
-
     }
 }
